c1b1530fc8081501a90b74f3b9c0516b9017f9f5
fabianhaef <fabian.haefi@gmail.com>
2021-01-17 16:51:22 +0100

    add products

---
+++ pages/products.js
@@ -0,0 +1,13 @@
+const Products = () => {
+    return (
+        <>
+            <h1>Cool Product 1</h1>
+            <p>This is a very cool product</p>
+            <p>$2.50</p>
+
+            <h1>Cool Product 2</h1>
+            <p>This is an even cooler product</p>
+            <p>$4.50</p>
+        </>
+    )
+}
